b7e5cb55789fc9d800beab059190abcda6a55edf
Mathe, Samuel <samuel.mathe@lexisnexisrisk.com>
2018-11-13 10:40:50 -0500

    DB dump 2018-11-13

---
+++ bipc_db/DUMP_11132018/seq_control2018-11-13 10-38-31.sql
@@ -0,0 +1,4 @@
+insert into `seq_control`(`obj_name`,`next_id`) values ('column_value_desc',25);
+insert into `seq_control`(`obj_name`,`next_id`) values ('contribution',345);
+insert into `seq_control`(`obj_name`,`next_id`) values ('contributor',17);
+insert into `seq_control`(`obj_name`,`next_id`) values ('table_column',5);
