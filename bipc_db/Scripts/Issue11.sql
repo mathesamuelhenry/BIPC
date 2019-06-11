10d82328603408dc1c0c92befccaee71ba42580d
Samuel Mathe <samuel.mathe@lexisnexisrisk.com>
2019-06-11 15:45:09 -0400

    Issue #11 db migration script.

---
+++ bipc_db/Scripts/Issue11.sql
@@ -0,0 +1,26 @@
+USE BIPC;
+
+CREATE TABLE account
+(
+   account_id             int(11) NOT NULL,
+   account_number         varchar(50) DEFAULT NULL,
+   account_name           varchar(100) DEFAULT NULL,
+   bank_name              varchar(50) DEFAULT NULL,
+   account_end_date       datetime DEFAULT NULL,
+   initial_balance        decimal(11, 2) NOT NULL,
+   status                 tinyint(4) DEFAULT '1',
+   date_added             datetime DEFAULT NULL,
+   date_changed           datetime DEFAULT NULL,
+   PRIMARY KEY(account_id)
+);
+
+ALTER TABLE contribution ADD COLUMN account_id int(11) DEFAULT NULL AFTER contribution_id;
+
+INSERT INTO seq_control VALUES ('account', 1);
+
+/* Run later */
+ALTER TABLE contribution MODIFY COLUMN account_id int(11) NOT NULL;
+
+ALTER TABLE contribution ADD CONSTRAINT FOREIGN KEY(account_id) REFERENCES account(account_id);
+
+
