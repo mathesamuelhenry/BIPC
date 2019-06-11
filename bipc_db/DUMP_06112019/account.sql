310f37c2dda1f339daf47d96f239e0b74d6f6c54
Samuel Mathe <samuel.mathe@lexisnexisrisk.com>
2019-06-11 16:25:25 -0400

    Issue #11 Dump of account table

---
+++ bipc_db/DUMP_06112019/account.sql
@@ -0,0 +1,3 @@
+USE bipc;
+
+insert into `account`(`account_id`,`account_number`,`account_name`,`bank_name`,`account_end_date`,`initial_balance`,`status`,`date_added`,`date_changed`) values (21,'2333456','BOFA_OLD','Bank Of America',null,75456.13,1,'2019-06-05 17:42:31','2019-06-08 13:46:03');
