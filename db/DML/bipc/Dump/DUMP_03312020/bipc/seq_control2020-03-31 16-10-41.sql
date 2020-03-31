f6a7d85a17b274c16f6323b277a64a6a3e3998e7
Samuel Mathe <msamuelhenry@outlook.com>
2020-03-31 17:32:58 -0400

    SQL DUMP : 03312020 backup

---
+++ db/DML/bipc/Dump/DUMP_03312020/bipc/seq_control2020-03-31 16-10-41.sql
@@ -0,0 +1,8 @@
+USE bipc;
+
+insert into `seq_control`(`obj_name`,`next_id`) values ('account',24);
+insert into `seq_control`(`obj_name`,`next_id`) values ('column_value_desc',296);
+insert into `seq_control`(`obj_name`,`next_id`) values ('contribution',6160);
+insert into `seq_control`(`obj_name`,`next_id`) values ('contributor',182);
+insert into `seq_control`(`obj_name`,`next_id`) values ('contributor_loan',25);
+insert into `seq_control`(`obj_name`,`next_id`) values ('table_column',61);
