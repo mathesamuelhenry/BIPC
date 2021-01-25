0a0cfcdc0445ea3b26798abfcbcdbbaee89219a1
Samuel Mathe <msamuelhenry@outlook.com>
2021-01-24 21:37:51 -0500

    Checking in latest DB dump

---
+++ db/DML/bipc/Dump/DUMP_20210124/contributor2021-01-24 21-36-13.sql
@@ -0,0 +1,26 @@
+USE bipc;
+
+insert into `contributor`(`contributor_id`,`first_name`,`last_name`,`family_name`,`status`,`date_added`,`date_changed`) values (21,'SAMUEL','MATHE',null,1,'2017-07-24 18:11:14','2018-01-09 11:21:36');
+insert into `contributor`(`contributor_id`,`first_name`,`last_name`,`family_name`,`status`,`date_added`,`date_changed`) values (31,'BIJU','VARGHESE',null,1,'2018-01-09 11:21:52',null);
+insert into `contributor`(`contributor_id`,`first_name`,`last_name`,`family_name`,`status`,`date_added`,`date_changed`) values (41,'DANIEL','KULANGARA',null,1,'2018-01-09 11:22:11',null);
+insert into `contributor`(`contributor_id`,`first_name`,`last_name`,`family_name`,`status`,`date_added`,`date_changed`) values (51,'SAM','DAVID',null,1,'2018-01-09 11:22:44',null);
+insert into `contributor`(`contributor_id`,`first_name`,`last_name`,`family_name`,`status`,`date_added`,`date_changed`) values (61,'JOJI','THOMAS',null,1,'2018-01-09 11:22:45',null);
+insert into `contributor`(`contributor_id`,`first_name`,`last_name`,`family_name`,`status`,`date_added`,`date_changed`) values (71,'MARIA','SIVAKUMAR',null,1,'2018-01-09 11:22:45',null);
+insert into `contributor`(`contributor_id`,`first_name`,`last_name`,`family_name`,`status`,`date_added`,`date_changed`) values (81,'ERSKINE','ROOPA',null,1,'2018-01-09 11:23:06',null);
+insert into `contributor`(`contributor_id`,`first_name`,`last_name`,`family_name`,`status`,`date_added`,`date_changed`) values (91,'MARY','GABRIEL',null,1,'2018-01-09 11:23:50',null);
+insert into `contributor`(`contributor_id`,`first_name`,`last_name`,`family_name`,`status`,`date_added`,`date_changed`) values (101,'REJI','PAREL',null,1,'2018-01-09 11:24:39',null);
+insert into `contributor`(`contributor_id`,`first_name`,`last_name`,`family_name`,`status`,`date_added`,`date_changed`) values (111,'ANJU','TITUS',null,1,'2018-01-09 11:24:39',null);
+insert into `contributor`(`contributor_id`,`first_name`,`last_name`,`family_name`,`status`,`date_added`,`date_changed`) values (121,'TIJU','THOMAS',null,1,'2018-01-09 11:24:39',null);
+insert into `contributor`(`contributor_id`,`first_name`,`last_name`,`family_name`,`status`,`date_added`,`date_changed`) values (122,'Sarah','Solomon','Mathe',0,null,'2019-08-11 21:44:13');
+insert into `contributor`(`contributor_id`,`first_name`,`last_name`,`family_name`,`status`,`date_added`,`date_changed`) values (124,'Samuel','Henry','Mathe',0,null,'2019-08-11 21:44:26');
+insert into `contributor`(`contributor_id`,`first_name`,`last_name`,`family_name`,`status`,`date_added`,`date_changed`) values (131,'JOJY & SONA','GEE VARGHESE',null,1,'2018-01-09 11:25:35','2020-01-05 04:40:54');
+insert into `contributor`(`contributor_id`,`first_name`,`last_name`,`family_name`,`status`,`date_added`,`date_changed`) values (141,'SHERRIN','DANIEL',null,1,'2018-01-09 12:12:30',null);
+insert into `contributor`(`contributor_id`,`first_name`,`last_name`,`family_name`,`status`,`date_added`,`date_changed`) values (151,'PAS. MATTHEWS','ITTY',null,1,'2018-01-09 12:16:06',null);
+insert into `contributor`(`contributor_id`,`first_name`,`last_name`,`family_name`,`status`,`date_added`,`date_changed`) values (161,'MATHEW','EAPEN',null,1,'2018-01-16 11:57:06',null);
+insert into `contributor`(`contributor_id`,`first_name`,`last_name`,`family_name`,`status`,`date_added`,`date_changed`) values (171,'GEORGE ','VARGHESE',null,1,'2018-01-20 15:48:51',null);
+insert into `contributor`(`contributor_id`,`first_name`,`last_name`,`family_name`,`status`,`date_added`,`date_changed`) values (181,'OBI','VARGHESE',null,1,'2018-12-27 19:29:34',null);
+insert into `contributor`(`contributor_id`,`first_name`,`last_name`,`family_name`,`status`,`date_added`,`date_changed`) values (183,'JAISON','JAMES',null,1,'2020-01-05 04:39:03','2020-01-05 04:40:54');
+insert into `contributor`(`contributor_id`,`first_name`,`last_name`,`family_name`,`status`,`date_added`,`date_changed`) values (184,'TEST','TEST',null,0,'2020-05-30 22:21:51','2020-05-31 22:17:46');
+insert into `contributor`(`contributor_id`,`first_name`,`last_name`,`family_name`,`status`,`date_added`,`date_changed`) values (185,'asdas','asdasda',null,0,'2020-05-31 20:47:37','2020-05-31 22:17:55');
+insert into `contributor`(`contributor_id`,`first_name`,`last_name`,`family_name`,`status`,`date_added`,`date_changed`) values (186,'ROBIN & SHERRIN','THOMAS',null,1,'2020-06-08 01:44:08',null);
+insert into `contributor`(`contributor_id`,`first_name`,`last_name`,`family_name`,`status`,`date_added`,`date_changed`) values (187,'PRINCE ','SAMUEL',null,1,'2020-11-29 18:06:51',null);
---
+++ db/DML/bipc/Dump/DUMP_20210124/seq_control2021-01-24 21-36-14.sql
@@ -0,0 +1,8 @@
+USE bipc;
+
+insert into `seq_control`(`obj_name`,`next_id`) values ('account',24);
+insert into `seq_control`(`obj_name`,`next_id`) values ('column_value_desc',328);
+insert into `seq_control`(`obj_name`,`next_id`) values ('contribution',6385);
+insert into `seq_control`(`obj_name`,`next_id`) values ('contributor',186);
+insert into `seq_control`(`obj_name`,`next_id`) values ('contributor_loan',25);
+insert into `seq_control`(`obj_name`,`next_id`) values ('table_column',61);
